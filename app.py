3614a8a61247ce67a9e90604bd2bf01f3e378f30
ahmedgidado <ahmedgidado@gmail.com>
2025-06-23 02:55:02 -0700

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ st.markdown(
     """
     <style>
     body {
-        background-color: #23272F; /* Dark background */
+        background-color: #ffffff; /* Dark background */
         color: white;
     }
     </style>
